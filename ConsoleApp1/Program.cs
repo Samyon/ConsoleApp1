444726d41bd02a28066bfa88550001dace037680
Семён Соловьёв <Samyon@mail.ru>
2023-10-23 05:02:36 +0700

    working program

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,32 +8,62 @@ using System.Xml.Linq;
 
 
 
-Console.WriteLine("Исходный алгоритм");
-Point3d wantedPoint = new Point3d(5, 5, 5);
-FirstSolve.Solve
-Console.WriteLine("Погрешность X = " + (x - wantedPoint.X));
-Console.WriteLine("Погрешность Y = " + (y - wantedPoint.Y));
-Console.WriteLine("Погрешность Z = " + (z - wantedPoint.Z));
+Tests tests = new Tests();
+tests.PrepareTest(new Point3d(5, 5, 5));
+tests.PrepareTest(new Point3d(-5, 5, 5));
+tests.PrepareTest(new Point3d(5, -5, 5));
+tests.PrepareTest(new Point3d(5, 5, -5));
+tests.PrepareTest(new Point3d(-5, -5, 5));
+tests.PrepareTest(new Point3d(5, -5, -5));
+tests.PrepareTest(new Point3d(-5, -5, 5));
+tests.PrepareTest(new Point3d(-5, -5, -5));
+tests.PrepareTest(new Point3d(57777, 57777, 57777));
+tests.PrepareTest(new Point3d(0, 57777, 57777));
+tests.PrepareTest(new Point3d(9999999, 57777, 57777));
+tests.PrepareTest(new Point3d(57777, 0, -57777));
 
 
+tests.Go();
 
 
-Console.WriteLine("");
-Console.WriteLine("");
-Console.WriteLine("");
-Console.WriteLine("Модифицированный алгоритм");
-var s = new Solve();
-s.Solve1(wantedPoint);
-
-public static class Tests
+public class Tests
 {
-    public static void Go(List<Point3d> Pints3d)
+    public class TestPoint3d
     {
+        public Point3d Point3dWanted;
+        public Point3d Point3dSolve1;
+        public Point3d Point3dSolve2;
+    }
 
+    private List<TestPoint3d> testPoints3d = new List<TestPoint3d>();
 
+    public void PrepareTest(Point3d wantedPoint)
+    {
+        TestPoint3d testPoint3d = new TestPoint3d();
+        testPoint3d.Point3dWanted = wantedPoint;
+        testPoint3d.Point3dSolve1 = FirstSolve.Solve(wantedPoint);
+        testPoint3d.Point3dSolve2 = SecondSolve.Solve(wantedPoint);
+        testPoints3d.Add(testPoint3d);
+    }
 
+    public void Go()
+    {
+        foreach (var item in testPoints3d)
+        {
+            Console.WriteLine("Искомая точка.  X=" + item.Point3dWanted.X + "  Y=" + item.Point3dWanted.Y + "  Z=" + item.Point3dWanted.Z);
+            Console.WriteLine("Исходный алгоритм. Погрешн." +
+                "  X=" + (item.Point3dWanted.X - item.Point3dSolve1.X) +
+                "  Y=" + (item.Point3dWanted.Y - item.Point3dSolve1.Y) +
+                "  Z=" + (item.Point3dWanted.Z - item.Point3dSolve1.Z));
+            Console.WriteLine("Модифиц. алгоритм. Погрешн." +
+                "  X=" + (item.Point3dWanted.X - item.Point3dSolve2.X) +
+                "  Y=" + (item.Point3dWanted.Y - item.Point3dSolve2.Y) +
+                "  Z=" + (item.Point3dWanted.Z - item.Point3dSolve2.Z));
+        }
     }
 }
+
+
 
 public class Point3d
 {
@@ -68,7 +98,7 @@ public static class FirstSolve
         var test = Distance.Get(wantedPoint, expectedPoint);
         if (test < 0.0000000001)
         {
-            return expectedPoint;           
+            return expectedPoint;
         }
         else
         {
@@ -77,50 +107,33 @@ public static class FirstSolve
     }
 }
 
-public class SecondSolve
+public static class SecondSolve
 {
-    public Point3d Solve(Point3d orig)
+    public static Point3d Solve(Point3d wantedPoint)
     {
-        var point = new OrigPoint(orig.X, orig.Y, orig.Z);
-        var a = point.Distance(0, 0, 0);
-
         //Задаём сферу
-        if (a == 0) return new Point3d { X = 0, Y = 0, Z = 0 };
+        var a = Distance.Get(wantedPoint, new Point3d(0, 0, 0));
+        //Если попали в точку - выдаём координаты
+        if (a == 0) return new Point3d(0, 0, 0);
 
         //Рисуем на сфере круг
-        var b = point.Distance(a, 0, 0);
-        if (b == 0) return new Point3d { X = a, Y = 0, Z = 0 };
-
+        var b = Distance.Get(wantedPoint, new Point3d(a, 0, 0));
+        //Если попали в точку - выдаём координаты
+        if (b == 0) return new Point3d(a, 0, 0);
         var x = (2 * a * a - b * b) / (2 * a);
-        //var xOb = a - x;
-        //var y = Math.Sqrt(b * b - xOb * xOb);
 
         //Рисуем на сфере круг
-        var c = point.Distance(0, a, 0);
-        if (c == 0) return new Point3d { X = 0, Y = c, Z = 0 };
+        var c = Distance.Get(wantedPoint, new Point3d(0, a, 0));
+        //Если попали в точку - выдаём координаты
+        if (c == 0) return new Point3d(0, a, 0);
         var y = (2 * a * a - c * c) / (2 * a);
 
-        //var z = Math.Sqrt(b * b - y * y);
-        var d = point.Distance(0, 0, a);
+        var d = Distance.Get(wantedPoint, new Point3d(0, 0, a));
+        //Если попали в точку - выдаём координаты
+        if (d == 0) return new Point3d(0, 0, a);
         var z = (2 * a * a - d * d) / (2 * a);
 
-        Console.WriteLine("----------------------------- ");
-        Console.WriteLine("Погрешность X = " + (x - orig.X));
-        Console.WriteLine("Погрешность Y = " + (y - orig.Y));
-        Console.WriteLine("Погрешность Z = " + (z - orig.Z));
-
-        if (d == 0)
-            return new Point3d { X = x, Y = y, Z = z };
-        else
-            return new Point3d { X = x, Y = y, Z = -z };
-
-
-
-
-        //Console.WriteLine("Погрешность X = " + (x - point._x));
-        //Console.WriteLine("Погрешность Y = " + (y - point._y));
-        //Console.WriteLine("Погрешность Z = " + (z - point._z));
-        //return null;
+        return new Point3d(x, y, z);
 
     }
 
